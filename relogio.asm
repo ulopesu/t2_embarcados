114897f8d43a0436b886168678d5f114393c29b5
Usiel F. Lopes Jr <siel.lopes@sinteseconsultores.com.br>
2023-01-20 16:29:52 -0300

    Interface - OK

--- relogio.asm
+++ relogio.asm
@@ -26,6 +26,7 @@ segment code
     MOV     [ES:intr*4+2], CS
     MOV     WORD [ES:intr*4],relogio
     STI
+	call 	textos_fixos
 
 l1:
 	cmp 	byte [tique], 0
@@ -131,23 +132,149 @@ converte:
     ADD     AH, 30h
     mov 	byte [horario+1], AH
 
-	; ESCRECVE HORARIO (00:00:00)
-	mov     	cx,8			;numero de caracteres
-	mov     	bx,0
-	mov     	dh,14			;linha 0-29
-	mov     	dl,35			;coluna 0-79
-	mov			byte[cor], verde
-	l_w_horario:
-		call	cursor
-		mov     al,[bx+horario]
-		call	caracter
-		inc     bx			;proximo caracter
-		inc		dl			;avanca a coluna
-		loop    l_w_horario
+	; ESCREVE HORARIO (00:00:00)
+	call escreve_horario
 
 	pop     ds
 	pop     ax
 	ret  
+
+textos_fixos:
+	call escreve_textohora
+	call escreve_alunoturma
+	call escreve_titulomenu
+	call escreve_textox
+	call escreve_textoseg
+	call escreve_textomin
+	call escreve_textohrs
+	ret
+
+escreve_horario:
+	mov word[num_chars], 8
+	mov byte[linha_str], 14
+	mov byte[coluna_str], 38
+	xor bx, bx
+	loop_cp_horario:
+		mov al, byte[horario + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_horario
+	call escreve_str
+	ret
+
+escreve_textohora:
+	mov word[num_chars], 5
+	mov byte[linha_str], 14
+	mov byte[coluna_str], 33
+	xor bx, bx
+	loop_cp_textohora:
+		mov al, byte[texto_hora + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_textohora
+	call escreve_str
+	ret
+
+escreve_alunoturma:
+	mov word[num_chars], 52
+	mov byte[linha_str], 2
+	mov byte[coluna_str], 13
+	xor bx, bx
+	loop_cp_alunoturma:
+		mov al, byte[aluno_turma + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_alunoturma
+	call escreve_str
+	ret
+
+escreve_titulomenu:
+	mov word[num_chars], 15
+	mov byte[linha_str], 22
+	mov byte[coluna_str], 32
+	xor bx, bx
+	loop_cp_titulomenu:
+		mov al, byte[titulo_menu + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_titulomenu
+	call escreve_str
+	ret
+
+escreve_textox:
+	mov word[num_chars], 7
+	mov byte[linha_str], 24
+	mov byte[coluna_str], 17
+	xor bx, bx
+	loop_cp_textox:
+		mov al, byte[texto_x + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_textox
+	call escreve_str
+	ret
+
+escreve_textoseg:
+	mov word[num_chars], 53
+	mov byte[linha_str], 25
+	mov byte[coluna_str], 17
+	xor bx, bx
+	loop_cp_textoseg:
+		mov al, byte[texto_s + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_textoseg
+	call escreve_str
+	ret
+
+escreve_textomin:
+	mov word[num_chars], 52
+	mov byte[linha_str], 26
+	mov byte[coluna_str], 17
+	xor bx, bx
+	loop_cp_textomin:
+		mov al, byte[texto_m + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_textomin
+	call escreve_str
+	ret
+
+escreve_textohrs:
+	mov word[num_chars], 50
+	mov byte[linha_str], 27
+	mov byte[coluna_str], 17
+	xor bx, bx
+	loop_cp_textohrs:
+		mov al, byte[texto_h + bx]
+		mov byte[main_str + bx], al
+		inc bx
+		cmp bx, word[num_chars]
+		jl loop_cp_textohrs
+	call escreve_str
+	ret
+
+escreve_str:
+	mov     	cx, word[num_chars]						;numero de caracteres
+	mov     	bx, 0
+	mov     	dh, byte[linha_str]						;linha 0-29
+	mov     	dl, byte[coluna_str]					;coluna 0-79
+	mov			byte[cor], branco_intenso
+	loop_escreve_str:
+		call	cursor
+		mov     al, byte[bx+main_str]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+		loop    loop_escreve_str
+	ret
 
 ;***************************************************************************
 ;
@@ -208,6 +335,13 @@ caracter:
 
 
 segment data
+	; COFIGURAÇÃO PARA STRING GENERICA
+	num_chars		dw		0
+	linha_str		db		0
+	coluna_str		db		0
+	main_str		times 	500		db	0
+
+	; COFIGURAÇÃO DE CORES
 	cor		db		branco_intenso	;	I R G B COR
 	preto			equ		0		;	0 0 0 0 preto
 	azul			equ		1		;	0 0 0 1 azul
@@ -236,7 +370,14 @@ segment data
 	segundo			db  	0
 	minuto 			db  	0
 	hora 			db  	0
-	horario			db  	0,0,':',0,0,':',0,0,' ', 13,'$'
+	texto_hora    	db  	'Hora:'													; 5 caracteres
+	horario			db  	0,0,':',0,0,':',0,0,' ', 13,'$'							; 8 caracteres
+	aluno_turma    	db  	'TL_2022/2 - Usiel Ferreira Lopes Junior - Turma 06.1' 	; 52 caracteres
+	titulo_menu    	db  	'Menu de teclas:'										; 15 caracteres
+	texto_x    		db  	'x: sair'												; 7 caracteres
+	texto_s    		db  	's: para o contador dos segundos e aguarda novo valor.'	; 53 caracteres
+	texto_m    		db  	'm: para o contador dos minutos e aguarda novo valor.'	; 52 caracteres
+	texto_h    		db  	'h: para o contador das horas e aguarda novo valor.'	; 50 caracteres
 segment stack stack
     resb 256
 stacktop:
